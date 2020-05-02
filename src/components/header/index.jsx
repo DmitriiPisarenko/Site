9b79cc609d69be125b5e52d2e48de04ff22c00e6
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-02 18:57:57 +0600

    first commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './header.module.css';
+
+export default function Header () {
+  return <header className={styles.Header}>
+    <div className={styles.Logo}>
+      <div className={styles.Logo_rounded}>
+        <div>
+          <div className={styles.Ellipse}>
+          </div>
+          <div className={styles.Rounded12}>
+          </div>
+        </div>
+        <div className={styles.Rounded2}>
+        </div>
+        <div className={styles.Rounded3}>
+        </div>
+    </div>
+    <div className={styles.Crowdme}>
+      CROWD<span className={styles.Me}>ME</span>
+    </div>
+    </div>
+    <ul className={styles.Nav_list}>
+        <li>HOME</li>
+        <li>HOW IT WORKS</li>
+        <li>DISCOVER A PROJECT</li>
+        <li>BLOG</li>
+        <li>FIND OUT MORE</li>
+    </ul>
+    <div className={styles.Login_case}>
+      LOGIN
+    </div>
+  
+  </header>;
+}
