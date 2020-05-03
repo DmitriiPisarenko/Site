dfbe812abab503efb760ddc9681c1a997afce16c
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-03 18:24:22 +0600

    fix header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,34 +2,38 @@ import React from 'react';
 import styles from './header.module.css';
 
 export default function Header () {
-  return <header className={styles.Header}>
-    <div className={styles.Logo}>
-      <div className={styles.Logo_rounded}>
-        <div>
-          <div className={styles.Ellipse}>
-          </div>
-          <div className={styles.Rounded12}>
-          </div>
+  return (
+    <header className={styles.Header}>
+      <div className={styles.Container}>
+        <div className={styles.Logo}>
+          <div className={styles.Logo_rounded}>
+            <div>
+              <div className={styles.Ellipse}>
+              </div>
+              <div className={styles.Rounded12}>
+              </div>
+            </div>
+            <div className={styles.Rounded2}>
+            </div>
+            <div className={styles.Rounded3}>
+            </div>
         </div>
-        <div className={styles.Rounded2}>
+        <div className={styles.Crowdme}>
+          CROWD<span className={styles.Me}>ME</span>
         </div>
-        <div className={styles.Rounded3}>
         </div>
-    </div>
-    <div className={styles.Crowdme}>
-      CROWD<span className={styles.Me}>ME</span>
-    </div>
-    </div>
-    <ul className={styles.Nav_list}>
-        <li>HOME</li>
-        <li>HOW IT WORKS</li>
-        <li>DISCOVER A PROJECT</li>
-        <li>BLOG</li>
-        <li>FIND OUT MORE</li>
-    </ul>
-    <button className={styles.Login_case}>
-      LOGIN
-    </button>
+        <ul className={styles.Nav_list}>
+            <li>HOME</li>
+            <li>HOW IT WORKS</li>
+            <li>DISCOVER A PROJECT</li>
+            <li>BLOG</li>
+            <li>FIND OUT MORE</li>
+        </ul>
+        <button className={styles.Login_case}>
+          LOGIN
+        </button>
+      </div>
+    </header>
+  );
   
-  </header>;
 }
